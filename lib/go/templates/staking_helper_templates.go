b97ccdf3da227525f8bd57768818bf4dfdb57050
Max Daunarovich <max.daunarovich@gmail.com>
2020-09-08 20:18:42 +0300

    Scaffold staking helper templates file

---
+++ lib/go/templates/staking_helper_templates.go
@@ -0,0 +1,16 @@
+package templates
+
+import (
+	"strings"
+
+	"github.com/onflow/flow-core-contracts/lib/go/templates/internal/assets"
+)
+
+const (
+	stakingHelperCreateNew = "stakingHelper/create_staking_helper.cdc"
+	stakingHelperAbort = "stakingHelper/abort_staking_request.cdc"
+	stakingHelperTransferTokens = "stakingHelper/transfer_tokens.cdc"
+	stakingHelperUnbondStake = "stakingHelper/unbond_stake.cdc"
+	stakingHelperWithdrawRewards = "stakingHelper/withdraw_rewards.cdc"
+)
+
