794401d0e8806767bd809160c6ab52379a6d7c0e
Max Daunarovich <max.daunarovich@gmail.com>
2020-09-08 20:18:08 +0300

    Move common addresses and ReplaceAddresses method to service_templates file

--- lib/go/templates/service_templates.go
+++ lib/go/templates/service_templates.go
@@ -12,6 +12,10 @@ const (
 	filePath             = "../../../transactions/"
 	inspectFieldFilename = "inspect_field.cdc"
 	defaultField         = "transactionField"
+
+	defaultFTAddress        = "FUNGIBLETOKENADDRESS"
+	defaultFlowTokenAddress = "FLOWTOKENADDRESS"
+	defaultIDTableAddress   = "IDENTITYTABLEADDRESS"
 )
 
 // GenerateInspectFieldScript creates a script that reads
@@ -27,4 +31,29 @@ func GenerateInspectFieldScript(field string) []byte {
 	)
 
 	return []byte(code)
+}
+
+// ReplaceAddresses replaces the import address
+// and phase in scripts that return info about a specific node and phase
+func ReplaceAddresses(code, ftAddr, flowTokenAddr, idTableAddr string) string {
+
+	code = strings.ReplaceAll(
+		code,
+		"0x"+defaultFTAddress,
+		"0x"+ftAddr,
+	)
+
+	code = strings.ReplaceAll(
+		code,
+		"0x"+defaultFlowTokenAddress,
+		"0x"+flowTokenAddr,
+	)
+
+	code = strings.ReplaceAll(
+		code,
+		"0x"+defaultIDTableAddress,
+		"0x"+idTableAddr,
+	)
+
+	return code
 }
