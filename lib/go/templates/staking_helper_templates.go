88af870f59de0950629e249e62907ab8498ae6dd
Max Daunarovich <max.daunarovich@gmail.com>
2020-09-11 22:50:20 +0300

    Add GenerateStakingHelperDeployScript template

--- lib/go/templates/staking_helper_templates.go
+++ lib/go/templates/staking_helper_templates.go
@@ -3,6 +3,8 @@ package templates
 import "github.com/onflow/flow-core-contracts/lib/go/templates/internal/assets"
 
 const (
+	stakingHelperDeployContract = "stakingHelper/deploy_contract.cdc"
+
 	stakingHelperCreateNew = "stakingHelper/create_staking_helper.cdc"
 	stakingHelperAbort = "stakingHelper/abort_staking_request.cdc"
 	stakingHelperTransferTokens = "stakingHelper/transfer_tokens.cdc"
@@ -14,4 +16,11 @@ func GenerateCreateNewStaker(ftAddr, stakingHelperAddr string) []byte {
 	code := assets.MustAssetString(filePath + stakingHelperCreateNew)
 
 	return []byte(ReplaceHelperAddresses(code, ftAddr, "","", stakingHelperAddr))
+}
+
+func GenerateStakingHelperDeployScript(ftAddr, flowAddr, idTableAddr string ) []byte {
+	print(assets.AssetNames())
+	code := assets.MustAssetString(filePath + stakingHelperDeployContract )
+
+	return []byte(ReplaceAddresses(code, ftAddr, flowAddr, ""))
 }
