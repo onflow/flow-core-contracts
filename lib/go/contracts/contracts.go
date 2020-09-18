4a1129e0ca4cae60f7d430e107b0c67963d1a18c
Bastian Müller <bastian@axiomzen.co>
2020-09-18 13:57:08 -0700

    allow replacement of import addresses in contracts, re-export FT contract code

--- lib/go/contracts/contracts.go
+++ lib/go/contracts/contracts.go
@@ -3,6 +3,10 @@ package contracts
 //go:generate go run github.com/kevinburke/go-bindata/go-bindata -prefix ../../../contracts/... -o internal/assets/assets.go -pkg assets -nometadata -nomemcopy ../../../contracts/...
 
 import (
+	"strings"
+
+	ftcontracts "github.com/onflow/flow-ft/lib/go/contracts"
+
 	"github.com/onflow/flow-core-contracts/lib/go/contracts/internal/assets"
 )
 
@@ -11,33 +15,77 @@ const (
 	flowServiceAccountFilename = "../../../contracts/FlowServiceAccount.cdc"
 	flowTokenFilename          = "../../../contracts/FlowToken.cdc"
 	flowIdentityTableFilename  = "../../../contracts/epochs/FlowIdentityTable.cdc"
-	hexPrefix                  = "0x"
+	defaultFungibleTokenAddr   = "0xee82856bf20e2aa6"
+	defaultFlowTokenAddr       = "0x0ae53cb6e3f42a79"
+	defaultFlowFeesAddr        = "0xe5a8b7f23e8b548f"
 )
+
+// FungibleToken returns the FungibleToken contract interface.
+func FungibleToken() []byte {
+	return ftcontracts.FungibleToken()
+}
 
 // FlowToken returns the FlowToken contract. importing the
 //
 // The returned contract will import the FungibleToken contract from the specified address.
-func FlowToken() []byte {
+func FlowToken(fungibleTokenAddr string) []byte {
 	code := assets.MustAssetString(flowTokenFilename)
+
+	code = strings.ReplaceAll(
+		code,
+		defaultFungibleTokenAddr,
+		fungibleTokenAddr,
+	)
+
 	return []byte(code)
 }
 
 // FlowFees returns the FlowFees contract.
 //
-// The returned contract imports the FungibleToken and FlowToken
-// contracts from the default addresses.
-func FlowFees() []byte {
+// The returned contract will import the FungibleToken and FlowToken
+// contracts from the specified addresses.
+func FlowFees(fungibleTokenAddr, flowTokenAddr string) []byte {
 	code := assets.MustAssetString(flowFeesFilename)
+
+	code = strings.ReplaceAll(
+		code,
+		defaultFungibleTokenAddr,
+		fungibleTokenAddr,
+	)
+
+	code = strings.ReplaceAll(
+		code,
+		defaultFlowTokenAddr,
+		flowTokenAddr,
+	)
 
 	return []byte(code)
 }
 
 // FlowServiceAccount returns the FlowServiceAccount contract.
 //
-// The returned contract imports the FungibleToken, FlowToken and FlowFees
-// contracts from the default addresses.
-func FlowServiceAccount() []byte {
+// The returned contract will import the FungibleToken, FlowToken and FlowFees
+// contracts from the specified addresses.
+func FlowServiceAccount(fungibleTokenAddr, flowTokenAddr, flowFeesAddr string) []byte {
 	code := assets.MustAssetString(flowServiceAccountFilename)
+
+	code = strings.ReplaceAll(
+		code,
+		defaultFungibleTokenAddr,
+		fungibleTokenAddr,
+	)
+
+	code = strings.ReplaceAll(
+		code,
+		defaultFlowTokenAddr,
+		flowTokenAddr,
+	)
+
+	code = strings.ReplaceAll(
+		code,
+		defaultFlowFeesAddr,
+		flowFeesAddr,
+	)
 
 	return []byte(code)
 }
