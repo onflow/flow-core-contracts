b44b42dac1d3daa361e0dff03499e726e086714a
Rob Myers <rob@robmyers.org>
2020-10-03 08:53:18 -0700

    Implement the lockbox staking templates (as I was meant to). (#26)

--- lib/go/templates/lockbox_templates.go
+++ lib/go/templates/lockbox_templates.go
@@ -16,7 +16,7 @@ const (
 	stakeNewLockedTokensFilename         = "lockbox/staker/stake_new_tokens.cdc"
 	stakeLockedUnlockedTokensFilename    = "lockbox/staker/stake_unlocked_tokens.cdc"
 	stakeLockedRewardedTokensFilename    = "lockbox/staker/stake_rewarded_tokens.cdc"
-	unstakeLockedTokensFilename          = "lockbox/staker/request_unstake.cdc"
+	unstakeLockedTokensFilename          = "lockbox/staker/request_unstaking.cdc"
 	withdrawLockedUnlockedTokensFilename = "lockbox/staker/withdraw_unlocked_tokens.cdc"
 	withdrawLockedRewardedTokensFilename = "lockbox/staker/withdraw_rewarded_tokens.cdc"
 
@@ -37,7 +37,7 @@ const (
 )
 
 // ReplaceLockboxAddress replaces the import address
-// and phase in scripts that return info about a specific node and phase
+// and phase in scripts that return info about a specific node and phase.
 func ReplaceLockboxAddress(code, lockboxAddress string) string {
 
 	code = strings.ReplaceAll(
@@ -50,9 +50,73 @@ func ReplaceLockboxAddress(code, lockboxAddress string) string {
 }
 
 // GenerateCreateLockedNodeScript creates a script that creates a new
-// node request with locked tokens
+// node request with locked tokens.
 func GenerateCreateLockedNodeScript(lockboxAddr, proxyAddr string) []byte {
 	code := assets.MustAssetString(filePath + createLockedNodeFilename)
+
+	code = ReplaceLockboxAddress(code, lockboxAddr)
+
+	return []byte(ReplaceStakingProxyAddress(code, proxyAddr))
+}
+
+// GenerateStakeNewLockedTokensScript creates a script that stakes new
+// locked tokens.
+func GenerateStakeNewLockedTokensScript(lockboxAddr, proxyAddr string) []byte {
+	code := assets.MustAssetString(filePath + stakeNewLockedTokensFilename)
+
+	code = ReplaceLockboxAddress(code, lockboxAddr)
+
+	return []byte(ReplaceStakingProxyAddress(code, proxyAddr))
+}
+
+// GenerateStakeLockedUnlockedTokensScript creates a script that stakes
+// unlocked tokens.
+// The unusual name is to avoid a clash with idtables_staking_templates.go .
+func GenerateStakeLockedUnlockedTokensScript(lockboxAddr, proxyAddr string) []byte {
+	code := assets.MustAssetString(filePath + stakeLockedUnlockedTokensFilename)
+
+	code = ReplaceLockboxAddress(code, lockboxAddr)
+
+	return []byte(ReplaceStakingProxyAddress(code, proxyAddr))
+}
+
+// GenerateStakeLockedRewardedTokensScript creates a script that stakes
+// unlocked tokens.
+// The unusual name is to avoid a clash with idtables_staking_templates.go .
+func GenerateStakeLockedRewardedTokensScript(lockboxAddr, proxyAddr string) []byte {
+	code := assets.MustAssetString(filePath + stakeLockedUnlockedTokensFilename)
+
+	code = ReplaceLockboxAddress(code, lockboxAddr)
+
+	return []byte(ReplaceStakingProxyAddress(code, proxyAddr))
+}
+
+// GenerateUnstakeLockedTokensScript creates a script that unstakes
+// locked tokens.
+func GenerateUnstakeLockedTokensScript(lockboxAddr, proxyAddr string) []byte {
+	code := assets.MustAssetString(filePath + unstakeLockedTokensFilename)
+
+	code = ReplaceLockboxAddress(code, lockboxAddr)
+
+	return []byte(ReplaceStakingProxyAddress(code, proxyAddr))
+}
+
+// GenerateWithdrawLockedUnlockedTokensScript creates a script that requests
+// a withdrawal of unlocked tokens.
+// The unusual name is to avoid a clash with idtables_staking_templates.go .
+func GenerateWithdrawLockedUnlockedTokensScript(lockboxAddr, proxyAddr string) []byte {
+	code := assets.MustAssetString(filePath + withdrawLockedUnlockedTokensFilename)
+
+	code = ReplaceLockboxAddress(code, lockboxAddr)
+
+	return []byte(ReplaceStakingProxyAddress(code, proxyAddr))
+}
+
+// GenerateWithdrawLockedRewardedTokensScript creates a script that requests
+// a withdrawal of unlocked tokens.
+// The unusual name is to avoid a clash with idtables_staking_templates.go .
+func GenerateWithdrawLockedRewardedTokensScript(lockboxAddr, proxyAddr string) []byte {
+	code := assets.MustAssetString(filePath + withdrawLockedRewardedTokensFilename)
 
 	code = ReplaceLockboxAddress(code, lockboxAddr)
 
