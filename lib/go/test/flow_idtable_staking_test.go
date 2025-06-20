d6384736df268b7ef68b77ff25b3bb9d24ab26c0
vishal <1117327+vishalchangrani@users.noreply.github.com>
2025-06-20 14:44:54 -0700

    updating validation logic and adding more unit tests

--- lib/go/test/flow_idtable_staking_test.go
+++ lib/go/test/flow_idtable_staking_test.go
@@ -391,8 +391,36 @@ func TestIDTableRegistration(t *testing.T) {
 			idTableAddress,
 			IDTableSigner,
 			adminID,
-			// Invalid Networking Address: IP address instead of domain name
+			// Invalid Networking Address: IPv4 address instead of domain name
 			"123.234.56.23:3569",
+			adminNetworkingKey,
+			adminStakingKey,
+			adminStakingPOP,
+			amountToCommit,
+			committed[adminID],
+			1,
+			true)
+
+		registerNode(t, b, env,
+			idTableAddress,
+			IDTableSigner,
+			adminID,
+			// Invalid Networking Address: IPv6 address instead of domain name
+			"2001:0db8:85a3:0000:0000:8a2e:0370:7334:3569",
+			adminNetworkingKey,
+			adminStakingKey,
+			adminStakingPOP,
+			amountToCommit,
+			committed[adminID],
+			1,
+			true)
+
+		registerNode(t, b, env,
+			idTableAddress,
+			IDTableSigner,
+			adminID,
+			// Invalid Networking Address: hyphen in the TLD
+			"host.example-site",
 			adminNetworkingKey,
 			adminStakingKey,
 			adminStakingPOP,
